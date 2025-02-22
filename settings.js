20ea48e481726f2248ba9c11017e4708d0f484c1
VvsTwizzy <sentahlemanyama91@gmail.com>
2025-02-22 14:46:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "LEF3NG BOT 🕴🏽",
+  botOwner: 'lef3ng†🕴🏽', // Your name
+  ownerNumber: '27630092157', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
