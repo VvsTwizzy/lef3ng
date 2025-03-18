1fcc4c61dc0608bf69f1d58662d4f12555fbf051
VvsTwizzy <sentahlemanyama91@gmail.com>
2025-03-18 07:54:51 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'lef3ng†',
   author: '★LEFENG★',
-  botName: "LEF3NG BOT 🕴🏽",
+  botName: "*𝑳𝒆𝒇𝒆𝒏𝒈 😈†",
   botOwner: 'lef3ng†🕴🏽', // Your name
   ownerNumber: '27630092157', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
